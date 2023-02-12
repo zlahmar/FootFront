68270685d59b17fb5746bd7dd0719d66aadf19cc
gaston <pallasgaston@gmail.com>
2023-02-12 12:26:55 +0100

    V1 -- Images des liens

--- src/components/LienMenu.js
+++ src/components/LienMenu.js
@@ -2,8 +2,8 @@ import {Link} from "react-router-dom";
 
 function LienMenu({lien, texte}){
     return (
-        <li>
-            <img alt={texte} className="h-auto max-w-full" src="../assets/logo192.svg" />
+        <li  className="flex items-center justify-center" >
+            <img alt={texte} className="h-1/5 w-1/5" src="https://drive.google.com/uc?export=view&id=1FQ4YldL0FVFGamhg1mbUlAQBbm7iBS0_" />
             <Link  to={lien}  className="font-medium text-tiffanyBlue hover:underline ">{texte}</Link>
         </li>
     )
