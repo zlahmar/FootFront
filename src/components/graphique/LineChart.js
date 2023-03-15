e0ec248e2de74e3cc8950a7df49976b9eda643ae
sangmin <tkdals4723@gmail.com>
2023-03-15 12:49:37 +0100

    Line Chart Testing ...

--- src/components/graphique/LineChart.js
+++ src/components/graphique/LineChart.js
@@ -1,11 +1,56 @@
+import { useEffect, useRef } from "react";
 import '../../styles/index.css'
+import * as d3 from 'd3'
+
 
 function LineChart(props){
-    console.log("dataArray : ",props.array[0])
+    // console.log("dataArray : ",props.array[0])
+    const svgRef = useRef();
+
+    const createGraph = () => {
+    // # 1. Access data
+    let data = props.array 
+    let parseTime = d3.timeParse("%Y");
+
+    data.forEach((d) => {
+        d.date = parseTime(d.date);
+        d.value = +d.value;
+    });
+    console.log("data : ", data)
+
+
+    // # 2. Create chart dimensions
+    // set the dimensions and margins of the graph
+    var margin = { top: 20, right: 20, bottom: 50, left: 70 },
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;    // append the svg object to the body of the page
+    var svg = d3.select("body").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", `translate(${margin.left},     ${margin.top})`);
+
+    // # 3. Create scales
+       // Add X axis and Y axis
+    var x = d3.scaleTime().range([0, width]);
+    var y = d3.scaleLinear().range([height, 0]);   x.domain(d3.extent(data, (d) => { return d.date; }));
+    y.domain([0, d3.max(data, (d) => { return d.value; })]);   svg.append("g")
+    .attr("transform", `translate(0, ${height})`)
+    .call(d3.axisBottom(x));   svg.append("g")
+    .call(d3.axisLeft(y)); 
     
+    
+
+    }
+
+    useEffect(() => {
+        createGraph()
+    },[])
+
     return(
         <div className='text-white'>
-            Hello
+            <svg ref={svgRef}>
+            </svg>
         </div>
     )
 }
