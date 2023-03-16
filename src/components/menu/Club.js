51735f0dc18072f5d55e967a2b9121d68bc1f6ac
gaston <pallasgaston@gmail.com>
2023-03-16 14:35:57 +0100

    V1 -clubs - param et debut requete

--- src/components/menu/Club.js
+++ src/components/menu/Club.js
@@ -1,16 +1,41 @@
 import LEAGUES from "../../data/Constants"
-import { useParams } from "react-router";
+import {Component} from "react";
 
-function Club(){
-    const { id } = useParams();
-    fetch(`${LEAGUES.DATA}/${id}`)
-        .then(response => console.log(response))
-    return (
-        <div>
-            Appel a API
-            {id}
-        </div>
-    )
+const id = window.location.pathname.slice(-1);
+const URL = `${LEAGUES.DATA}/${id}`;
+
+class Club extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            nationalityData: {}
+        };
+    }
+
+    componentDidMount() {
+        this.fetchNationalityData();
+    }
+
+    fetchNationalityData() {
+        fetch(URL)
+            .then(response => response.json())
+            .then(data => {
+                const nameOriginal = data.nationality.name_original;
+                const id = data.nationality.id;
+                this.setState({ nationalityData: { nameOriginal, id } });
+            })
+            .catch(error => console.error(error));
+    }
+
+    render() {
+        return (
+            <div>
+                <p>Pays : {this.state.nationalityData.nameOriginal}</p>
+                <p>ID de la nationalité : {this.state.nationalityData.id}</p>
+            </div>
+        );
+    }
 }
 
-export default Club
+export default Club;
