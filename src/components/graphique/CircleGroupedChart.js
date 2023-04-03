e2ea30171b60ff34e0cfcf1f519040994c15097a
sangmin <tkdals4723@gmail.com>
2023-04-03 10:04:34 +0200

    corrected CircleGroupedChart

--- src/components/graphique/CircleGroupedChart.js
+++ src/components/graphique/CircleGroupedChart.js
@@ -1,17 +1,17 @@
 import { Bar } from '@nivo/bar'
 
-const keys = ['yellowcard', 'redcard']
+const keys = ['redcard', 'yellowcard']
 const commonProps = {
     margin : { top: 50, right: 130, bottom: 50, left: 60 },
     indexBy: 'league',
     keys,
     labelTextColor: 'inherit:darker(1.4)',
-    labelSkipWidth: 5,
+    labelSkipWidth: 10,
     labelSkipHeight: 10,
 }
 
 const CustomBarComponent = ({ bar: { x, y, width, height, color } }) => (
-    <circle cx={x + width / 2} cy={y + height / 2} r={Math.min(width, height) / 2} fill={color} />
+    <circle cx={x + width / 2} cy={y + height / 3} r={Math.min(width, height) / 2} fill={color} />
 )
 
 const CircleGroupedChart = ({data, width}) => (
@@ -22,9 +22,9 @@ const CircleGroupedChart = ({data, width}) => (
         textColor: 'white',
     }}
     width={width}
-    height={320}
+    height={350}
     data={data}
-    colors={['#ffea80', '#fa2a2a']}
+    colors={['#fa2a2a', '#ffea80']}
     {...commonProps} 
     />
 )
