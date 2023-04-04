3f373a3c8237a256710bfb9dce3aac70f83d5233
sangmin <tkdals4723@gmail.com>
2023-04-04 08:59:38 +0200

    padding adjusted

--- src/components/bloc/BlocCarte.js
+++ src/components/bloc/BlocCarte.js
@@ -3,7 +3,7 @@ import { Children } from 'react';
 
 function BlocCarte({children}){
     return (
-        <div className="bg-gunMetal pt-5 flex justify-center items-center rounded-3xl h-full">
+        <div className="bg-gunMetal flex justify-center items-center rounded-3xl h-full">
             <div className="flex flex-row flex-wrap justify-center w-4/5 ">
             {Children.map(children, child =>
                 <div className="lg:basis-1/3 md:basis-full sm:basis-full min-[320px]:basis-full text-center px-3 pb-3">        
--- src/components/bloc/BlocTitre.js
+++ src/components/bloc/BlocTitre.js
@@ -1,9 +1,9 @@
 
 function BlocTitre({text}){
     return (
-        <div className="flex pt-3 pb-3">
+        <div className="flex pt-3 pb-3 pl-3">
             <div className="flex flex-row flex-wrap w-4/5 ">
-                <p className="font-title text-white mb-3 text-xl">
+                <p className="font-title text-white text-xl">
                     {text}
                 </p>
             </div>
