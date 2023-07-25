27cc419c2292c5ac05d988c62584320776ebd9f1
Sangmin <tkdals4723@gmail.com>
2023-07-25 09:17:32 +0200

    Clean Code ING

--- src/components/carte/joueur/JoueurCarte.js
+++ src/components/carte/joueur/JoueurCarte.js
@@ -2,25 +2,20 @@ import '../../../styles/index.css'
 import {Link} from "react-router-dom";
 import player_card from '../../../assets/icon/player_card.png'
 
-function JoueurCarte({ isClickDisabled = true}){
+function JoueurCarte({ isClickDisabled = false}){
     return(
         <div className={`${isClickDisabled ? 'h-full' : ''}  py-5`}>
-            <Link
-                className={` ${isClickDisabled ? 'pointer-events-none h-full mb-5' : ''} bg-gunMetal `}
+            <div className="w-64 h-96 m-auto">
+                <Link
+                to={{pathname : "/joueurs",
+                state: { joueurId: "good" }}}
+                className={` bg-gunMetal `}
                 >
-                <div className="w-64 h-96 m-auto">
-                    <img className="" src={player_card} alt="" />
-                </div>
-                {/* <div className="flex items-start relative">
-                    <img className={`${isClickDisabled ? `w-20`: `w-14`}  rounded-2xl absolute left-1 -top-6 border-2 border-onyx`} src="#" alt="" />
-                </div>
-
-                <div className="w-full h-full p-1 sm:p-1 lg:p-3 text-center p-3 sm:p-5 lg:p-7  flex flex-col justify-center">
-                    <span className=" text-lg font-bold text-white font-title"></span>
-                </div> */}
-            </Link>
+                    <img className={` ${isClickDisabled ? 'pointer-events-none h-full mb-5' : ''} `} src={player_card} alt="" />
+                </Link>
+            </div>
         </div>
-    )
+        )
 }
 
 export default JoueurCarte
